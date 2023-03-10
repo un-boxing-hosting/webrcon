a0866530c3b296c97d2d3014c1bfa1802b9b0848
Brandon Valencia <brandonvalencia@live.com>
2023-03-09 21:39:00 -0800

    added additional convars
    added additional convars

--- js/convar.js
+++ js/convar.js
@@ -6,7 +6,42 @@ function ConvarListController($scope, rconService, $interval) {
 	//$scope.OrderBy = '-ConnectedSeconds';
 
 	$scope.Refresh = function () {
-		$scope.weather = ["rain","clouds","fog","wind"];
+		$scope.weather = ["rain","clouds","wind","fog","radiation"];
+	}
+
+	$scope.On = function (id) {
+		if (id == "rain") {
+			rconService.Command("weather."+id+" 100");
+			}
+		if (id == "clouds") {
+			rconService.Command("weather.cloud_coverage 100");
+		}
+		if (id == "wind") {
+			rconService.Command("weather.wind 100");
+		}
+		if (id == "radiation") {
+			rconService.Command("server.radiation true");
+		}
+		if (id == "fog") {
+			rconService.Command("weather.fog 1");
+		}
+	}
+	$scope.Off = function (id) {
+		if (id == "rain") {
+		rconService.Command("weather."+id+" 0");
+		}
+		if (id == "clouds") {
+		rconService.Command("weather.cloud_coverage 0");
+		}
+		if (id == "wind") {
+		rconService.Command("weather.wind 0");
+		}
+		if (id == "radiation") {
+			rconService.Command("server.radiation false");
+		}
+		if (id == "fog") {
+			rconService.Command("weather.fog 0");
+		}
 	}
 
 	$scope.Order = function (field) {

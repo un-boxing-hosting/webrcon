5062ff5bc7795e8bb1b674e4e447a0a52e1a9761
Brandon Valencia <brandonvalencia@live.com>
2023-03-11 22:00:59 -0800

    changed ban and kick functions
    changed ban and kick functions

--- js/playerlist.js
+++ js/playerlist.js
@@ -26,14 +26,26 @@ function PlayerListController($scope, rconService, $interval) {
 	}
 
 	$scope.KickPlayer = function (id) {
-		rconService.Command('kick ' + id);
+
+	var KickReason = window.prompt("Enter the reason for the kick:", "");
+	//if kick reason is empty or null, cancel the kick
+	if (KickReason == null || KickReason == "") {
+		//prompt player was not kicked
+		window.alert("Player was not kicked due to no reason", "");
+		return;
+	}
+		rconService.Command('kick ' + id + ' ' + KickReason);
 
 		$scope.Refresh();
 	}
 	$scope.BanPlayer = function (id, time, reason) {
 		// prompt for time and reason
-
-		
+		var BanReason = window.prompt("Enter the reason for the ban:", "");	
+		if (BanReason == null || BanReason == "") {
+			//prompt player was not ban
+			window.alert("Player was not banned due to no reason", "");
+			return;
+		}
 		rconService.Command('ban ' + id + ' ' + time + ' ' + reason);
 		console.log( 'ban ' + id + ' ' + time + ' ' + reason );
 		$scope.Refresh();

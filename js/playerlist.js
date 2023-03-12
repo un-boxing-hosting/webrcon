bf7ed0ed5e3ef93b803759c132a633879914ab02
un-boxing-man <unboxingman@hotmail.com>
2023-03-11 21:31:21 -0800

    add ban reason and time

--- js/playerlist.js
+++ js/playerlist.js
@@ -1,51 +1,45 @@
+app.controller('PlayerListController', PlayerListController);
 
-app.controller( 'PlayerListController',  PlayerListController );
-
-function PlayerListController( $scope, rconService, $interval )
-{
+function PlayerListController($scope, rconService, $interval) {
 	$scope.Output = [];
 	$scope.OrderBy = '-ConnectedSeconds';
 
-	$scope.Refresh = function ()
-	{
-		rconService.getPlayers($scope, function ( players )
-		{
+	$scope.Refresh = function () {
+		rconService.getPlayers($scope, function (players) {
 			$scope.Players = players;
 		});
 	}
 
-	$scope.Order = function( field )
-	{
-		if ( $scope.OrderBy === field )
-		{
+	$scope.Order = function (field) {
+		if ($scope.OrderBy === field) {
 			field = '-' + field;
 		}
 
 		$scope.OrderBy = field;
 	}
 
-	$scope.SortClass = function( field )
-	{
-		if ( $scope.OrderBy === field ) return "sorting";
-		if ( $scope.OrderBy === "-" + field ) return "sorting descending";
+	$scope.SortClass = function (field) {
+		if ($scope.OrderBy === field) return "sorting";
+		if ($scope.OrderBy === "-" + field) return "sorting descending";
 
 		return null;
 	}
 
-	$scope.KickPlayer = function ( id )
-	{
-		rconService.Command( 'kick ' + id );
+	$scope.KickPlayer = function (id) {
+		rconService.Command('kick ' + id);
 
 		$scope.Refresh();
 	}
-	$scope.BanPlayer = function ( id )
-	{
-		rconService.Command( 'ban ' + id );
+	$scope.BanPlayer = function (id, time, reason) {
+		// prompt for time and reason
 
+		
+		rconService.Command('ban ' + id + ' ' + time + ' ' + reason);
+		console.log( 'ban ' + id + ' ' + time + ' ' + reason );
 		$scope.Refresh();
 	}
 
-	rconService.InstallService( $scope, $scope.Refresh )
+	rconService.InstallService($scope, $scope.Refresh)
 
 	// var timer = $interval( function ()
 	// {
